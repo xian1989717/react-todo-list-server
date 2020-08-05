c6e3faabd2bdcb5423fe808b908270a9a66cecd8
刘述 <383883040@qq.com>
2020-08-05 11:29:56 +0800

    提交代码

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,16 @@
+const url = require('url')
+const { add: serverAdd } = require('../service/index')
+
+function add (params) {
+  return serverAdd(params)
+}
+
 module.exports.servers = function (server) {
   server.on('request', function (request, response) {
-    response.end('hello world')
+    const urls = request.url
+    const { pathname, query } = url.parse(urls, true)
+    if (pathname === '/add') {
+      response.send(add(query))
+    }
   })
 }
