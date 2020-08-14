1c7270b527a4365e801a54491c3de2ba66cfb947
刘述 <383883040>
2020-08-14 21:30:29 +0800

    提交代码

--- service/index.js
+++ service/index.js
@@ -1,4 +1,5 @@
 const { connection } = require('../mysql.config')
+
 module.exports.add = function (params, callback) {
   connection.connect()
   const sql = `insert into todo_list set name = ${params.name},is_removed = false,type = 'doing'`
@@ -11,6 +12,15 @@ module.exports.add = function (params, callback) {
 module.exports.select = function (params, callback) {
   connection.connect()
   const sql = `select * from todo_list`
+  connection.query(sql, function (err, data) {
+    callback(data)
+    connection.end()
+  })
+}
+
+module.exports.delete = function (params, callback) {
+  connection.connect()
+  const sql = `update tdo_list set is_removed = 1 where id=`params
   connection.query(sql, function (err, data) {
     callback(data)
     connection.end()
