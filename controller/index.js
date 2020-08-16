eb081987ec08737d4b4c11ca7e8dd78b51c8f7e5
刘述 <383883040>
2020-08-16 20:07:19 +0800

    提交代码

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,5 @@
 const url = require('url')
-const { add, select } = require('../service/index')
+const { add, select, delete: deleteDate, update } = require('../service/index')
 
 module.exports.servers = function (server) {
   server.on('request', function (request, response) {
@@ -12,6 +12,14 @@ module.exports.servers = function (server) {
     } else if (pathname === '/select') {
       select(query, function (data) {
         response.end(JSON.stringify(data))
+      })
+    } else if (pathname === '/delete') {
+      deleteDate(query, function (data) {
+        response.end(Boolean(data.affectedRows).toString())
+      })
+    } else if (pathname === '/update') {
+      update(query, (data) => {
+        response.end(Boolean(data.affectedRows).toString())
       })
     }
   })
--- service/index.js
+++ service/index.js
@@ -1,28 +1,38 @@
 const { connection } = require('../mysql.config')
 
-module.exports.add = function (params, callback) {
-  connection.connect()
-  const sql = `insert into todo_list set name = ${params.name},is_removed = false,type = 'doing'`
-  connection.query(sql, function (err, data) {
-    callback(data)
-    connection.end()
-  })
-}
-
-module.exports.select = function (params, callback) {
-  connection.connect()
-  const sql = `select * from todo_list`
-  connection.query(sql, function (err, data) {
-    callback(data)
-    connection.end()
-  })
-}
-
-module.exports.delete = function (params, callback) {
-  connection.connect()
-  const sql = `update tdo_list set is_removed = 1 where id=`params
-  connection.query(sql, function (err, data) {
-    callback(data)
-    connection.end()
-  })
+module.exports = {
+  add (params, callback) {
+    connection.connect()
+    const sql = `insert into todo_list set name = ${params.name},is_removed = false,type = 'doing'`
+    connection.query(sql, function (err, data) {
+      callback(data)
+      connection.end()
+    })
+  },
+  select (params, callback) {
+    connection.connect()
+    const sql = `select * from todo_list where is_removed = false`
+    connection.query(sql, function (err, data) {
+      callback(data)
+      connection.end()
+    })
+  },
+  delete (params, callback) {
+    const { id } = params
+    connection.connect()
+    const sql = 'update todo_list set is_removed = 1 where id=' + id
+    connection.query(sql, function (err, data) {
+      callback(data)
+      connection.end()
+    })
+  },
+  update (params, callback) {
+    const { id } = params
+    connection.connect()
+    const sql = `update todo_list set type = 'done' where id=${id}`
+    connection.query(sql, function (err, data) {
+      callback(data)
+      connection.end()
+    })
+  }
 }
